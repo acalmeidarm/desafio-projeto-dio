8fec8ab82bc9d683c2abc8a3da75095a9424832c
Ana Carolina Ribeiro <acalmeidarm@gmail.com>
2022-10-27 18:06:06 -0300

    inclusao de exercicio/desafio referente a aula 01 de javascript

---
+++ modulo_javascript/VariaveisOperadores/aula01.js
@@ -0,0 +1,15 @@
+//Variaveis
+
+//let é uma variável
+//const é um valor constante. O valor da const não é substituido
+
+//Operadores
+
+//let variavel = 10 + 10 ;
+//let variavel = 10 * 20 ;
+//let variavel = 50 - 10 ;
+//let variavel = (100 / 10) ;
+//console.log(variavel);
+
+
+
---
+++ modulo_javascript/VariaveisOperadores/desafio_aula01.js
@@ -0,0 +1,18 @@
+/*Desafio:  Faça um programa para calcular o valor de uma viagem
+Serão necessárias 03 variaveis:
+    var 01 - preço do combustivel
+    var 02 - gasto médio de combustivel do carro por KM
+    var 03 - distância em KM da viagem
+Imprima no console o valor que será gasto para realizar esta viagem
+*/
+
+let precoCombustivel = 5.09
+let qtdKmLitro = 12;
+let distancia = 1500;
+let consumoCarro = distancia/qtdKmLitro;
+let gastoTotal = consumoCarro * precoCombustivel;
+
+console.log("Você precisará de: " + consumoCarro + " litros de combustivel")
+console.log("O valor de combustivel gasto na viagem será de " + gastoTotal)
+
+
